1e545e0e10a33df3107baaedd11c5851ebde2549
Heather Peterson <hpeterson462@gmail.com>
2021-02-25 17:22:54 -0800

    sql setup

---
+++ sql/setup.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS daycares;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    user_location TEXT NOT NULL
+);
+
+CREATE TABLE daycares (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT REFERENCES users(id),
+    name TEXT NOT NULL,
+    street_address TEXT NOT NULL,
+    state TEXT NOT NULL,
+    image TEXT NOT NULL
+);
+
